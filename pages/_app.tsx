2e957c711c9fa45c17a25ba6d5431207dcc53c35
Teemu <squigothic@gmail.com>
2021-05-16 22:23:13 +0300

    init the project and config eslint + prettier

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
+
+function Ruuvi2({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default Ruuvi2
